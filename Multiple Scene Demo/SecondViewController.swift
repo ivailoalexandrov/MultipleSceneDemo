f274749440f125ba8d74184dc8e35cd120f6b163
Ivailo Alexandrov <ivailo.alexandrov@icloud.com>
2020-02-08 10:59:06 +0200

    Initial Commit

---
+++ Multiple Scene Demo/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  Multiple Scene Demo
+//
+//  Created by Ivailo Alexandrov on 2.02.20.
+//  Copyright © 2020 Ivailo Alexandrov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Second VC"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Multiple Scene Demo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Multiple Scene Demo
+//
+//  Created by Ivailo Alexandrov on 2.02.20.
+//  Copyright © 2020 Ivailo Alexandrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "First VC"
+    }
+
+
+}
+
